c3f35920763160695bc0a4fb82225a66482000a5
Andrew Piper <piper.andrew@spcollege.edu>
2016-11-07 14:32:49 -0500

    Converted source to typescript.

---
+++ Views/editComponents/source.ts
@@ -0,0 +1,49 @@
+﻿"use strict";
+
+
+import * as domQuery from "hr.domquery";
+import * as storage from "hr.storage";
+import * as controller from "hr.controller";
+import * as navmenu from "hr.widgets.navmenu";
+import * as pageService from "clientlibs.PageService";
+
+var CodeMirror = (<any>window).CodeMirror;
+
+class EditSourceController {
+    private editSourceDialog;
+    private cm;
+
+    constructor(bindings: controller.BindingCollection) {
+        this.editSourceDialog = bindings.getToggle('dialog');
+        var codemirrorElement = domQuery.first('#editSourceTextarea');
+        this.cm = CodeMirror.fromTextArea(codemirrorElement, {
+            lineNumbers: true,
+            mode: "htmlmixed",
+            theme: "edity"
+        });
+    }
+
+    apply(evt) {
+        evt.preventDefault();
+        this.editSourceDialog.off();
+        pageService.setHtml(this.cm.getValue());
+    }
+
+    startEdit() {
+        this.editSourceDialog.on();
+        this.cm.setSize(null, window.innerHeight - 250);
+        this.cm.setValue(pageService.getHtml());
+        setTimeout(() => this.cm.refresh(), 500);
+    }
+}
+
+var editSourceController = controller.create<EditSourceController, void, void>("editSource", EditSourceController)[0];
+
+class NavItemController {
+    edit() {
+        editSourceController.startEdit();
+    }
+}
+
+var editMenu = navmenu.getNavMenu("edit-nav-menu-items");
+editMenu.add("EditSourceNavItem", NavItemController);
