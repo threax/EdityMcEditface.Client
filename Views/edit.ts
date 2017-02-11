0bb37eebc421c69ea618b3759b411508dcde705e
Andrew Piper <andrew@threax.com>
2017-02-10 19:45:45 -0500

    Build a real controller for edit tools.

--- Views/edit.ts
+++ Views/edit.ts
@@ -4,6 +4,8 @@ import * as component from "hr.components";
 import * as domQuery from "hr.domquery";
 import * as navmenu from "edity.editorcore.navmenu";
 
+import * as controller from 'hr.controller';
+
 var navMenu = domQuery.first('[data-editor-navmenu]');
 
 function itemAdded(item) {
@@ -13,3 +15,24 @@ function itemAdded(item) {
 var menu = navmenu.getNavMenu("edit-nav-menu-items");
 menu.getItems().forEach(itemAdded);
 menu.itemAdded.add(itemAdded);
+
+class ToolsViewController {
+    public static Builder() {
+        return new controller.ControllerBuilder<ToolsViewController, void, void>(ToolsViewController);
+    }
+
+    private mainToggle: controller.OnOffToggle;
+
+    constructor(bindings: controller.BindingCollection) {
+        this.mainToggle = bindings.getToggle("main");
+    }
+
+    public toggleTools(evt: Event) {
+        evt.preventDefault();
+        this.mainToggle.toggle();
+    }
+}
+
+
+var builder = ToolsViewController.Builder();
+builder.create("edity-tools");
