22bbe4cabfe356d4cc964268287080f8fe4bc6ac
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-17 09:43:40 -0400

    Updating to new build system.

--- build.js
+++ build.js
@@ -1,173 +1,326 @@
 ﻿"use strict";
+var copy = require('threax-npm-tk/copy');
+var less = require('threax-npm-tk/less');
 
-var gulp = require("gulp");
-var path = require('path');
+module.exports = function (outDir, iconOutPath) {
 
-var compileTypescript = require('threax-gulp-tk/typescript.js');
-var compileLess = require('threax-gulp-tk/less.js');
-var copyFiles = require('threax-gulp-tk/copy.js');
-var clientBuild = require(__dirname + '/clientbuild.js');
+    var promises = [];
 
-module.exports = function (rootDir, webroot, settings) {
+    //promises.push(less({
+    //    encoding: 'utf8',
+    //    importPaths: [__dirname, __dirname + '/../bootstrap/less'],
+    //    inFile: __dirname + '/SpcBootstrap.less',
+    //    outFile: outDir + "/bootstrap/dist/css/SpcBootstrap.css",
+    //    compress: true,
+    //}));
 
-    var htmlRapierBuild = require(rootDir + '/node_modules/htmlrapier/build');
-    var htmlRapierWidgetsBuild = require(rootDir + '/node_modules/htmlrapier.widgets/build');
-    var htmlRapierBootstrapBuild = require(rootDir + '/node_modules/htmlrapier.bootstrap/build');
+    //Copy favicon
+    //var faviconPromise = copy.file(
+    //    __dirname + '/favicon.ico',
+    //    iconOutPath + '/favicon.ico'
+    //);
 
-    var libDir = webroot + "/lib";
-    var viewBaseDir = webroot + "/edity";
-    var editylibDir = viewBaseDir + "/lib";
+    //File Copy, get the fonts
+    //promises.push(copy.dir(
+    //    __dirname + "/../bootstrap/dist/fonts",
+    //    outDir + "/bootstrap/dist/fonts"
+    //));
 
-    if (settings === undefined) {
-        settings = {};
-    }
+    //File Copy, images
+    //var imagePromise = copy.dir(
+    //    __dirname + '/Images',
+    //    outDir + '/SpcBootstrap/Images'
+    //);
 
-    var concat = true;
-    if (settings.concat !== undefined) {
-        concat = settings.concat;
-    }
+    promises.push(less({
+        encoding: 'utf8',
+        importPaths: [__dirname, __dirname + '/../bootstrap/less'],
+        input: __dirname + '/edity/**/*.less',
+        basePath: __dirname,
+        out: outDir + '/edity',
+        compress: true,
 
-    var minify = true;
-    if (settings.minify !== undefined) {
-        minify = settings.minify;
-    }
 
-    //Shared client side
-    copyFiles({
-        libs: ["./node_modules/jsns/jsns.js",
-        ],
-        baseName: './node_modules/jsns',
-        dest: libDir
-    });
+        //files: [
+        //__dirname + '/edity/**/*.less'
+        //],
+        //dest: libDir + '/edity',
+        //importPaths: path.join(__dirname),
+    }));
 
-    htmlRapierBuild(__dirname, libDir, settings);
-    htmlRapierWidgetsBuild(__dirname, libDir, settings);
-    htmlRapierBootstrapBuild(__dirname, libDir, settings);
+//    //Compile widget typescript
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/Widgets/**/*.ts",
+//            "!**/*.intellisense.js"
+//        ],
+//        runners: false,
+//        dest: libDir + '/Widgets',
+//        sourceRoot: __dirname + "/Widgets/",
+//        namespace: "edity.widgets",
+//        concat: false,
+//        minify: settings.minify
+//    });
 
-    compileLess({
-        files: [
-        __dirname + '/bootstrap/bootstrap-custom.less'
-        ],
-        dest: libDir + '/bootstrap/dist/css',
-        importPaths: path.join(rootDir + '/node_modules/bootstrap/less'),
-    });
+//    //Edity Editor Client Side
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/codemirror/lib/**/*",
+//               rootDir + "/node_modules/codemirror/mode/xml/**/*",
+//               rootDir + "/node_modules/codemirror/mode/javascript/**/*",
+//               rootDir + "/node_modules/codemirror/mode/css/**/*",
+//               rootDir + "/node_modules/codemirror/mode/htmlmixed/**/*",
+//               rootDir + "/node_modules/codemirror/addon/merge/**/*" 
+//        ],
+//        baseName: rootDir + '/node_modules',
+//        dest: editylibDir
+//    });
 
-    copyFiles({
-        libs: [rootDir + "/node_modules/bootstrap/dist/**/*",
-               "!" + rootDir + "/node_modules/bootstrap/dist/css/**/*",
-               "!" + rootDir + "/node_modules/bootstrap/dist/js/**/*"
-        ],
-        baseName: rootDir + '/node_modules',
-        dest: libDir
-    });
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/ckeditor/ckeditor.js",
+//               rootDir + "/node_modules/ckeditor/contents.css",
+//               rootDir + "/node_modules/ckeditor/skins/moono/**/*",
+//               rootDir + "/node_modules/ckeditor/lang/en.js",
+//               rootDir + "/node_modules/ckeditor/plugins/*.png",
+//               rootDir + "/node_modules/ckeditor/plugins/magicline/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/colorbutton/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/panelbutton/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/floatpanel/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/dialog/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/specialchar/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/clipboard/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/link/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/table/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/pastefromword/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/uploadwidget/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/notificationaggregator/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/filetools/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/widget/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/lineutils/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/notification/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/image/**/*",
+//        ],
+//        baseName: rootDir + '/node_modules',
+//        dest: editylibDir
+//    });
 
-    compileLess({
-        files: [
-        __dirname + '/edity/**/*.less'
-        ],
-        dest: libDir + '/edity',
-        importPaths: path.join(__dirname),
-    });
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/ckeditor-youtube-plugin/youtube/**/*"],
+//        baseName: rootDir + '/node_modules/ckeditor-youtube-plugin',
+//        dest: editylibDir + "/ckeditor/plugins/"
+//    });
 
-    //Compile widget typescript
-    compileTypescript({
-        libs: [
-            __dirname + "/Widgets/**/*.ts",
-            "!**/*.intellisense.js"
-        ],
-        runners: false,
-        dest: libDir + '/Widgets',
-        sourceRoot: __dirname + "/Widgets/",
-        namespace: "edity.widgets",
-        concat: false,
-        minify: settings.minify
-    });
+//    copyFiles({
+//        libs: [
+//            __dirname + "/diff_match_patch/**/*",
+//            __dirname + "/ckeditor/**/*",
+//            __dirname + "/codemirror/**/*",
+//            "!**/*.intellisense.js",
+//            "!**/*.less"],
+//        baseName: __dirname,
+//        dest: editylibDir
+//    });
 
-    //Edity Editor Client Side
-    copyFiles({
-        libs: [rootDir + "/node_modules/codemirror/lib/**/*",
-               rootDir + "/node_modules/codemirror/mode/xml/**/*",
-               rootDir + "/node_modules/codemirror/mode/javascript/**/*",
-               rootDir + "/node_modules/codemirror/mode/css/**/*",
-               rootDir + "/node_modules/codemirror/mode/htmlmixed/**/*",
-               rootDir + "/node_modules/codemirror/addon/merge/**/*" 
-        ],
-        baseName: rootDir + '/node_modules',
-        dest: editylibDir
-    });
+//    //Editor Core ts
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/EditorCore/**/*.ts",
+//            "!" + __dirname + "/EditorCore/EditorPageStart.ts",
+//        ],
+//        runners: ["edity.config"],
+//        dest: editylibDir,
+//        sourceRoot: __dirname + "/EditorCore/",
+//        namespace: "edity.editorcore",
+//        output: "EditorCore",
+//        concat: settings.concat,
+//        minify: settings.minify
+//    });
 
-    copyFiles({
-        libs: [rootDir + "/node_modules/ckeditor/ckeditor.js",
-               rootDir + "/node_modules/ckeditor/contents.css",
-               rootDir + "/node_modules/ckeditor/skins/moono/**/*",
-               rootDir + "/node_modules/ckeditor/lang/en.js",
-               rootDir + "/node_modules/ckeditor/plugins/*.png",
-               rootDir + "/node_modules/ckeditor/plugins/magicline/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/colorbutton/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/panelbutton/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/floatpanel/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/dialog/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/specialchar/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/clipboard/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/link/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/table/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/pastefromword/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/uploadwidget/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/notificationaggregator/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/filetools/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/widget/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/lineutils/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/notification/**/*",
-               rootDir + "/node_modules/ckeditor/plugins/image/**/*",
-        ],
-        baseName: rootDir + '/node_modules',
-        dest: editylibDir
-    });
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/EditorCore/EditorPageStart.ts",
+//        ],
+//        runners: null,
+//        dest: editylibDir,
+//        sourceRoot: __dirname + "/EditorCore/",
+//        namespace: "edity.editorcore",
+//        output: "EditorCorePageStart",
+//        concat: settings.concat,
+//        minify: settings.minify
+//    });
 
-    copyFiles({
-        libs: [rootDir + "/node_modules/ckeditor-youtube-plugin/youtube/**/*"],
-        baseName: rootDir + '/node_modules/ckeditor-youtube-plugin',
-        dest: editylibDir + "/ckeditor/plugins/"
-    });
+    return Promise.all(promises);
+}
 
-    copyFiles({
-        libs: [
-            __dirname + "/diff_match_patch/**/*",
-            __dirname + "/ckeditor/**/*",
-            __dirname + "/codemirror/**/*",
-            "!**/*.intellisense.js",
-            "!**/*.less"],
-        baseName: __dirname,
-        dest: editylibDir
-    });
+//"use strict";
 
-    //Editor Core ts
-    compileTypescript({
-        libs: [
-            __dirname + "/EditorCore/**/*.ts",
-            "!" + __dirname + "/EditorCore/EditorPageStart.ts",
-        ],
-        runners: ["edity.config"],
-        dest: editylibDir,
-        sourceRoot: __dirname + "/EditorCore/",
-        namespace: "edity.editorcore",
-        output: "EditorCore",
-        concat: settings.concat,
-        minify: settings.minify
-    });
+//var gulp = require("gulp");
+//var path = require('path');
 
-    compileTypescript({
-        libs: [
-            __dirname + "/EditorCore/EditorPageStart.ts",
-        ],
-        runners: null,
-        dest: editylibDir,
-        sourceRoot: __dirname + "/EditorCore/",
-        namespace: "edity.editorcore",
-        output: "EditorCorePageStart",
-        concat: settings.concat,
-        minify: settings.minify
-    });
+//var compileTypescript = require('threax-gulp-tk/typescript.js');
+//var compileLess = require('threax-gulp-tk/less.js');
+//var copyFiles = require('threax-gulp-tk/copy.js');
+//var clientBuild = require(__dirname + '/clientbuild.js');
 
-    clientBuild(settings, __dirname, webroot, "site.client");
-};
+//module.exports = function (rootDir, webroot, settings) {
+
+//    var htmlRapierBuild = require(rootDir + '/node_modules/htmlrapier/build');
+//    var htmlRapierWidgetsBuild = require(rootDir + '/node_modules/htmlrapier.widgets/build');
+//    var htmlRapierBootstrapBuild = require(rootDir + '/node_modules/htmlrapier.bootstrap/build');
+
+//    var libDir = webroot + "/lib";
+//    var viewBaseDir = webroot + "/edity";
+//    var editylibDir = viewBaseDir + "/lib";
+
+//    if (settings === undefined) {
+//        settings = {};
+//    }
+
+//    var concat = true;
+//    if (settings.concat !== undefined) {
+//        concat = settings.concat;
+//    }
+
+//    var minify = true;
+//    if (settings.minify !== undefined) {
+//        minify = settings.minify;
+//    }
+
+//    //Shared client side
+//    copyFiles({
+//        libs: ["./node_modules/jsns/jsns.js",
+//        ],
+//        baseName: './node_modules/jsns',
+//        dest: libDir
+//    });
+
+//    htmlRapierBuild(__dirname, libDir, settings);
+//    htmlRapierWidgetsBuild(__dirname, libDir, settings);
+//    htmlRapierBootstrapBuild(__dirname, libDir, settings);
+
+//    compileLess({
+//        files: [
+//        __dirname + '/bootstrap/bootstrap-custom.less'
+//        ],
+//        dest: libDir + '/bootstrap/dist/css',
+//        importPaths: path.join(rootDir + '/node_modules/bootstrap/less'),
+//    });
+
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/bootstrap/dist/**/*",
+//               "!" + rootDir + "/node_modules/bootstrap/dist/css/**/*",
+//               "!" + rootDir + "/node_modules/bootstrap/dist/js/**/*"
+//        ],
+//        baseName: rootDir + '/node_modules',
+//        dest: libDir
+//    });
+
+//    compileLess({
+//        files: [
+//        __dirname + '/edity/**/*.less'
+//        ],
+//        dest: libDir + '/edity',
+//        importPaths: path.join(__dirname),
+//    });
+
+//    //Compile widget typescript
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/Widgets/**/*.ts",
+//            "!**/*.intellisense.js"
+//        ],
+//        runners: false,
+//        dest: libDir + '/Widgets',
+//        sourceRoot: __dirname + "/Widgets/",
+//        namespace: "edity.widgets",
+//        concat: false,
+//        minify: settings.minify
+//    });
+
+//    //Edity Editor Client Side
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/codemirror/lib/**/*",
+//               rootDir + "/node_modules/codemirror/mode/xml/**/*",
+//               rootDir + "/node_modules/codemirror/mode/javascript/**/*",
+//               rootDir + "/node_modules/codemirror/mode/css/**/*",
+//               rootDir + "/node_modules/codemirror/mode/htmlmixed/**/*",
+//               rootDir + "/node_modules/codemirror/addon/merge/**/*" 
+//        ],
+//        baseName: rootDir + '/node_modules',
+//        dest: editylibDir
+//    });
+
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/ckeditor/ckeditor.js",
+//               rootDir + "/node_modules/ckeditor/contents.css",
+//               rootDir + "/node_modules/ckeditor/skins/moono/**/*",
+//               rootDir + "/node_modules/ckeditor/lang/en.js",
+//               rootDir + "/node_modules/ckeditor/plugins/*.png",
+//               rootDir + "/node_modules/ckeditor/plugins/magicline/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/colorbutton/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/panelbutton/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/floatpanel/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/dialog/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/specialchar/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/clipboard/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/link/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/table/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/pastefromword/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/uploadwidget/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/notificationaggregator/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/filetools/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/widget/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/lineutils/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/notification/**/*",
+//               rootDir + "/node_modules/ckeditor/plugins/image/**/*",
+//        ],
+//        baseName: rootDir + '/node_modules',
+//        dest: editylibDir
+//    });
+
+//    copyFiles({
+//        libs: [rootDir + "/node_modules/ckeditor-youtube-plugin/youtube/**/*"],
+//        baseName: rootDir + '/node_modules/ckeditor-youtube-plugin',
+//        dest: editylibDir + "/ckeditor/plugins/"
+//    });
+
+//    copyFiles({
+//        libs: [
+//            __dirname + "/diff_match_patch/**/*",
+//            __dirname + "/ckeditor/**/*",
+//            __dirname + "/codemirror/**/*",
+//            "!**/*.intellisense.js",
+//            "!**/*.less"],
+//        baseName: __dirname,
+//        dest: editylibDir
+//    });
+
+//    //Editor Core ts
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/EditorCore/**/*.ts",
+//            "!" + __dirname + "/EditorCore/EditorPageStart.ts",
+//        ],
+//        runners: ["edity.config"],
+//        dest: editylibDir,
+//        sourceRoot: __dirname + "/EditorCore/",
+//        namespace: "edity.editorcore",
+//        output: "EditorCore",
+//        concat: settings.concat,
+//        minify: settings.minify
+//    });
+
+//    compileTypescript({
+//        libs: [
+//            __dirname + "/EditorCore/EditorPageStart.ts",
+//        ],
+//        runners: null,
+//        dest: editylibDir,
+//        sourceRoot: __dirname + "/EditorCore/",
+//        namespace: "edity.editorcore",
+//        output: "EditorCorePageStart",
+//        concat: settings.concat,
+//        minify: settings.minify
+//    });
+
+//    clientBuild(settings, __dirname, webroot, "site.client");
+//};
