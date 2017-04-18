56ee43308fabc3193b2c1b35adb3297040187b45
Andrew Piper <piper.andrew@spcollege.edu>
2017-04-18 10:58:33 -0400

    Moving to typescript build system, working pretty well so far.

---
+++ Build/build.ts
@@ -0,0 +1,64 @@
+﻿"use strict";
+var copy = require('threax-npm-tk/copy');
+var less = require('threax-npm-tk/less');
+var clientBuild = require('./clientbuild');
+
+var fs = require('threax-npm-tk/node_modules/fs-extra');
+
+var filesDir = __dirname + "/..";
+
+export function build(outDir, iconOutPath, moduleDir) {
+    var promises = [];
+
+    promises.push(clientBuild(outDir, iconOutPath, moduleDir));
+    promises.push(buildCkEditor(outDir + '/edity/lib', moduleDir));
+    promises.push(buildCodemirror(outDir + '/edity/lib', moduleDir));
+
+    //Return composite promise
+    return Promise.all(promises);
+}
+
+function buildCkEditor(outDir, moduleDir): Promise<any> {
+    var promises: Promise<any>[] = [];
+
+    //ckeditor
+    promises.push(copy.glob(moduleDir + "/ckeditor/ckeditor.js", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/contents.css", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/skins/moono/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/lang/en.js", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/*.png", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/magicline/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/colorbutton/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/panelbutton/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/floatpanel/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/dialog/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/specialchar/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/clipboard/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/link/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/table/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/pastefromword/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/uploadwidget/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/notificationaggregator/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/filetools/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/widget/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/lineutils/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/notification/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/ckeditor/plugins/image/**/*", moduleDir, outDir));
+
+    return Promise.all(promises);
+}
+
+function buildCodemirror(outDir, moduleDir): Promise<any> {
+    var promises: Promise<any>[] = [];
+
+    //Codemirror
+    promises.push(copy.glob(moduleDir + "/codemirror/lib/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/codemirror/mode/xml/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/codemirror/mode/javascript/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/codemirror/mode/css/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/codemirror/mode/htmlmixed/**/*", moduleDir, outDir));
+    promises.push(copy.glob(moduleDir + "/codemirror/addon/merge/**/*", moduleDir, outDir));
+    promises.push(copy.glob(filesDir + "/codemirror/theme/edity.css", filesDir, outDir));
+
+    return Promise.all(promises);
+}
--- Build/clientbuild.ts
+++ Build/clientbuild.ts
@@ -2,52 +2,48 @@
 var copy = require('threax-npm-tk/copy');
 var less = require('threax-npm-tk/less');
 
-module.exports = function (outDir, iconOutPath, moduleDir) {
-    var promises = [];
-
-    promises.push(less({
+module.exports = async function (outDir, iconOutPath, moduleDir) {
+    await less({
         encoding: 'utf8',
         importPaths: [__dirname, moduleDir + '/bootstrap/less'],
         input: __dirname + '/edity/**/*.less',
         basePath: __dirname + '/edity',
         out: outDir + '/edity',
         compress: true,
-    }));
+    });
 
-    promises.push(less({
+    await less({
         encoding: 'utf8',
         importPaths: [__dirname, moduleDir + '/bootstrap/less'],
         input: __dirname + '/Views/**/*.less',
         basePath: __dirname + '/Views',
         out: outDir + '/edity/layouts',
         compress: true,
-    }));
+    });
 
     //Views
-    promises.push(copy.glob(
+    await copy.glob(
         __dirname + '/Views/**/*.css',
         __dirname + '/Views',
         outDir + '/edity/layouts'
-    ));
+    );
 
-    promises.push(copy.glob(
+    await copy.glob(
         __dirname + '/Views/**/*.html',
         __dirname + '/Views',
         outDir + '/edity/layouts'
-    ));
+    );
 
     //Templates
-    promises.push(copy.glob(
+    await copy.glob(
         __dirname + '/Templates/**/*.css',
         __dirname + '/Templates',
         outDir + '/edity/Templates'
-    ));
+    );
 
-    promises.push(copy.glob(
+    await copy.glob(
         __dirname + '/Templates/**/*.html',
         __dirname + '/Templates',
         outDir + '/edity/Templates'
-    ));
-
-    return Promise.all(promises);
+    );
 }
