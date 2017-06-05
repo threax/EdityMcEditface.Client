ef60c1b57d65867acac438e8b1236ec09237e02d
Andrew Piper <piper.andrew@spcollege.edu>
2017-06-05 15:34:19 -0400

    Made the edit menu stay open.

--- Views/edit.ts
+++ Views/edit.ts
@@ -7,21 +7,46 @@ import * as domQuery from "hr.domquery";
 import * as navmenu from "edity.editorcore.navmenu";
 import * as controller from 'hr.controller';
 import * as toggle from 'hr.toggles';
+import * as storage from "hr.storage";
 
 class ToolsViewController {
     public static get InjectorArgs(): controller.DiFunction<any>[] {
-        return [controller.BindingCollection];
+        return [controller.BindingCollection, ToolsViewStorage];
     }
 
     private mainToggle: controller.OnOffToggle;
 
-    constructor(bindings: controller.BindingCollection) {
+    constructor(bindings: controller.BindingCollection, private storage: ToolsViewStorage) {
         this.mainToggle = bindings.getToggle("main");
+
+        var status = this.storage.getValue({
+            open: false
+        });
+
+        if (status.open) {
+            this.mainToggle.on();
+        }
+
+        window.addEventListener("beforeunload", e => {
+            this.storage.setValue({
+                open: this.mainToggle.mode
+            });
+        });
     }
 
     public toggleTools(evt: Event) {
         evt.preventDefault();
         this.mainToggle.toggle();
+    }
+}
+
+interface ToolsViewSessionData {
+    open: boolean;
+}
+
+export class ToolsViewStorage extends storage.JsonStorage<ToolsViewSessionData> {
+    constructor(storageDriver: storage.IStorageDriver) {
+        super(storageDriver)
     }
 }
 
@@ -37,4 +62,5 @@ menu.itemAdded.add(itemAdded);
 
 var builder = new controller.InjectedControllerBuilder();
 builder.Services.addTransient(ToolsViewController, ToolsViewController);
+builder.Services.tryAddTransient(ToolsViewStorage, s => new ToolsViewStorage(new storage.SessionStorageDriver("toolsViewStorage")));
 builder.create("edity-tools", ToolsViewController);
