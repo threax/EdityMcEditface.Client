c2ec8801b5368fbcbd98496d918b4c5dc0b5c61c
Andrew Piper <andrew@threax.com>
2021-01-05 17:48:31 -0500

    Setup htmlrapier correctly for latest version.

--- Views/default.ts
+++ Views/default.ts
@@ -3,12 +3,15 @@
 import * as TreeMenu from 'hr.treemenu.TreeMenu';
 import * as EditModeDetector from 'edity.editorcore.EditModeDetector';
 import * as controller from 'hr.controller';
-import * as bootstrap from 'hr.bootstrap.all';
+import * as bootstrap from 'hr.bootstrap.main';
 import * as SidebarToggle from 'hr.sidebar.sidebartoggle';
 import * as fetcher from 'hr.fetcher';
+import * as hr from 'hr.main';
 import * as windowFetch from 'hr.windowfetch';
 
-bootstrap.activate();
+//Activate htmlrapier
+hr.setup();
+bootstrap.setup();
 SidebarToggle.activate();
 
 //Only create tree menu if not in edit mode, otherwise the editor will create an editing tree menu instead
