b07cc5f89c200996f98fa2bf04d6326bb6d5d0c8
Andrew Piper <andrew@threax.com>
2021-07-22 16:40:58 -0400

    Fix preview.

--- Views/editComponents/preview.ts
+++ Views/editComponents/preview.ts
@@ -1,4 +1,4 @@
-﻿import * as navmenu from '../../EditorCore/navmenu";
+﻿import * as navmenu from '../../EditorCore/navmenu';
 
 var editMenu = navmenu.getNavMenu("edit-nav-menu-items");
 editMenu.addInjected("PreviewButton", navmenu.PageInfoStart + 0, undefined);
